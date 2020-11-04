180b982723b3340af370bf1793b28fd83efda3e2
matsjsk <matsjorgen@hotmail.com>
2020-11-04 09:56:48 +0100

    feat: add security and endpoint permissions

---
+++ src/main/kotlin/no/fdk/dataset_catalog/security/SecurityConfig.kt
@@ -0,0 +1,51 @@
+package no.fdk.dataset_catalog.security
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
+import org.springframework.security.web.util.matcher.RequestMatcher
+import javax.servlet.http.HttpServletRequest
+
+@Configuration
+open class SecurityConfig : WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity) {
+        http.csrf().disable()
+
+        http.cors()
+            .and()
+            .authorizeRequests()
+            .requestMatchers(TurtleMatcher())
+            .permitAll()
+            .antMatchers(HttpMethod.OPTIONS)
+            .permitAll()
+            .antMatchers(HttpMethod.GET,"/ping")
+            .permitAll()
+            .antMatchers(HttpMethod.GET,"/ready")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .oauth2ResourceServer()
+            .jwt()
+    }
+
+    @Bean
+    open fun jwtDecoder(properties: OAuth2ResourceServerProperties): JwtDecoder? {
+        val jwtDecoder = NimbusJwtDecoder.withJwkSetUri(properties.jwt.jwkSetUri).build()
+        jwtDecoder.setJwtValidator(DelegatingOAuth2TokenValidator(AudienceValidator()))
+        return jwtDecoder
+    }
+}
+
+private class TurtleMatcher: RequestMatcher{
+    override fun matches(request: HttpServletRequest?): Boolean =
+        request?.getHeader("Accept") != null &&
+            request.getHeader("Accept") == "text/turtle" &&
+            request.method == "GET"
+}
