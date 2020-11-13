b985d73e99429f0dedc7ea795a5eae151fa9fc70
matsjsk <matsjorgen@hotmail.com>
2020-11-13 11:17:32 +0100

    feat: adds publisher validation to create

--- src/main/kotlin/no/fdk/dataset_catalog/service/OrganizationService.kt
+++ src/main/kotlin/no/fdk/dataset_catalog/service/OrganizationService.kt
@@ -1,26 +1,64 @@
 package no.fdk.dataset_catalog.service
 
 import no.fdk.dataset_catalog.model.Organization
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
-import org.springframework.http.MediaType
+import org.springframework.http.*
 import org.springframework.stereotype.Service
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.ResourceAccessException
+import org.springframework.web.client.RestClientException
 import org.springframework.web.client.RestTemplate
 
+private val logger = LoggerFactory.getLogger(OrganizationService::class.java)
 
 @Service
-class OrganizationService() {
+class OrganizationService {
+    private val restTemplate = RestTemplate()
+    private val defaultHeaders = HttpHeaders()
     @Value("\${application.openDataEnhetProxy}")
-    private val openDataEnhetsregisteretProxy: String? = null
+    private val organizationCatalogueUrl: String? = null
+
+    init {
+        defaultHeaders.accept = listOf(MediaType.APPLICATION_JSON)
+    }
 
     fun getByOrgNr(orgnr: String): Organization? {
-        val url = openDataEnhetsregisteretProxy + orgnr
+        val url = organizationCatalogueUrl + orgnr
+        defaultHeaders.accept = listOf(MediaType.APPLICATION_JSON)
+        val entity: HttpEntity<String> = HttpEntity("body", defaultHeaders)
+        return try {
+            restTemplate.exchange(url, HttpMethod.GET, entity, Organization::class.java).body
+        } catch (e: Exception) {
+            logger.error("Getting organization by ID $orgnr failed.", e)
+            return null
+        }
+    }
+
+    fun hasDelegationPermission(id: String): Boolean {
+        val allowDelegatedRegistration: Boolean? = getOrganization(id)?.allowDelegatedRegistration
+        return allowDelegatedRegistration != null && allowDelegatedRegistration
+    }
+
+    private fun getOrganization(organizationNumber: String?): Organization? {
         val restTemplate = RestTemplate()
-        val headers = HttpHeaders()
-        headers.accept = listOf(MediaType.APPLICATION_JSON)
-        val entity: HttpEntity<String> = HttpEntity("body", headers)
-        return restTemplate.exchange(url, HttpMethod.GET, entity, Organization::class.java).body
+
+        return try {
+            restTemplate.exchange(String.format("%s/organizations/%s", organizationCatalogueUrl, organizationNumber),
+                HttpMethod.GET,
+                HttpEntity<Any?>(defaultHeaders),
+                Organization::class.java)
+                .body
+
+        } catch (exception: HttpClientErrorException) {
+            if (exception.statusCode == HttpStatus.NOT_FOUND) {
+                throw Exception(String.format("Organization with ID %s not found", organizationNumber), exception)
+            }
+            throw Exception("An unexpected client error occurred", exception)
+        } catch (exception: ResourceAccessException) {
+            throw Exception("Downstream service not available", exception)
+        } catch (exception: RestClientException) {
+            throw Exception("An unexpected server error occurred", exception)
+        }
     }
 }
