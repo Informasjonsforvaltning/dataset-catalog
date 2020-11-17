aa3cc1f333adece8239019d5f870e3ed071a32ac
matsjsk <matsjorgen@hotmail.com>
2020-11-17 12:43:04 +0100

    feat: removes unused exception

--- src/main/kotlin/no/fdk/dataset_catalog/webutils/exceptions/FDKException.kt
+++
@@ -1,8 +0,0 @@
-package no.fdk.dataset_catalog.webutils.exceptions
-
-abstract class FDKException : Exception {
-    internal constructor() : super() {}
-    internal constructor(message: String?) : super(message) {}
-    internal constructor(message: String?, cause: Throwable?) : super(message, cause) {}
-    internal constructor(cause: Throwable?) : super(cause) {}
-}
