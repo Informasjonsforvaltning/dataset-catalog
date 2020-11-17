4f7ebde866b366ae3699d5cc9e3d7856bd7b5ac6
matsjsk <matsjorgen@hotmail.com>
2020-11-17 12:57:10 +0100

    feat: adds configuration properties

--- src/main/kotlin/no/fdk/dataset_catalog/service/ConceptService.kt
+++ src/main/kotlin/no/fdk/dataset_catalog/service/ConceptService.kt
@@ -2,9 +2,9 @@ package no.fdk.dataset_catalog.service
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import no.fdk.dataset_catalog.configuration.ApplicationProperties
 import no.fdk.dataset_catalog.model.Concept
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import java.io.BufferedReader
@@ -15,8 +15,7 @@ private val logger = LoggerFactory.getLogger(ConceptService::class.java)
 
 @Service
 class ConceptService(
-    @Value("\${application.conceptCatalogueHost}")
-    private val conceptCatalogueHost: String? = null
+    private val applicationProperties: ApplicationProperties
 ) {
 
     fun getConcepts(ids: List<String>): List<Concept> {
@@ -24,22 +23,22 @@ class ConceptService(
     }
 
     private fun getConcept(id: String): Concept? {
-    URL("$conceptCatalogueHost/concepts/$id")
-        .openConnection()
-        .run {
-            this as HttpURLConnection
-            if (responseCode != HttpStatus.OK.value()) {
-                logger.error("Error: $responseCode")
-                return null
-            }
-            val jsonBody = inputStream.bufferedReader().use(BufferedReader::readText)
-            return try {
-                jacksonObjectMapper().readValue(jsonBody)
-            } catch (t: Throwable) {
-                logger.error("Unable to parse response from concept catalogue for '$id'")
-                null
+        URL("${applicationProperties.conceptCatalogueHost}/concepts/$id")
+            .openConnection()
+            .run {
+                this as HttpURLConnection
+                if (responseCode != HttpStatus.OK.value()) {
+                    logger.error("Error: $responseCode")
+                    return null
+                }
+                val jsonBody = inputStream.bufferedReader().use(BufferedReader::readText)
+                return try {
+                    jacksonObjectMapper().readValue(jsonBody)
+                } catch (t: Throwable) {
+                    logger.error("Unable to parse response from concept catalogue for '$id'")
+                    null
+                }
             }
         }
-    }
 
 }
