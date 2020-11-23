94a9c5cd64725533187e73418569eaae631abb8f
matsjsk <matsjorgen@hotmail.com>
2020-11-23 10:33:06 +0100

    fix: gets all datasets for catalog

--- src/main/kotlin/no/fdk/dataset_catalog/service/RDFService.kt
+++ src/main/kotlin/no/fdk/dataset_catalog/service/RDFService.kt
@@ -11,34 +11,36 @@ import org.springframework.stereotype.Service
 
 
 @Service
-class RDFService(val catalogService: CatalogService) {
+class RDFService(private val catalogService: CatalogService,
+                 private val datasetService: DatasetService) {
 
     fun getAll(): Model? = catalogService.getAll().createCatalogModel()
 
     fun getById(catalogId: String): Model? =
         catalogService.getByID(catalogId)
             ?.let { listOf(it).createCatalogModel() }
-}
 
-private fun List<Catalog>.createCatalogModel(): Model {
-    val model = ModelFactory.createDefaultModel()
-    model.setNsPrefix("dct", DCTerms.getURI())
-    model.setNsPrefix("dcat", DCAT.getURI())
+    private fun List<Catalog>.createCatalogModel(): Model {
+        val model = ModelFactory.createDefaultModel()
+        model.setNsPrefix("dct", DCTerms.getURI())
+        model.setNsPrefix("dcat", DCAT.getURI())
 
-    forEach{
-        model.createResource(it.uri)
-            .addProperty(RDF.type, DCAT.catalog)
-            .safeAddPropertyByLang(DCTerms.title, it.title)
-            .safeAddPropertyByLang(DCTerms.description, it.description)
-            .safeAddLinkedProperty(DCTerms.publisher, it.publisher?.uri)
-            .safeAddDateLiteral(DCTerms.issued, it.issued)
-            .safeAddDateLiteral(DCTerms.modified, it.modified)
-            .safeAddProperty(DCTerms.language, it.language)
-            .safeAddDatasetListLinkedProperty(DCAT.dataset, it.dataset)
-            .addPublisher(it.publisher)
+        forEach{
+            model.createResource(it.uri)
+                .addProperty(RDF.type, DCAT.catalog)
+                .safeAddPropertyByLang(DCTerms.title, it.title)
+                .safeAddPropertyByLang(DCTerms.description, it.description)
+                .safeAddLinkedProperty(DCTerms.publisher, it.publisher?.uri)
+                .safeAddDateLiteral(DCTerms.issued, it.issued)
+                .safeAddDateLiteral(DCTerms.modified, it.modified)
+                .safeAddProperty(DCTerms.language, it.language)
+                .safeAddDatasetListLinkedProperty(DCAT.dataset, it.dataset)
+                .addPublisher(it.publisher)
 
-        it.dataset?.forEach { dataset ->  model.createDatasetResource(dataset) }
+            it.id?.let {catId -> datasetService.getAll(catId).forEach { dataset ->  model.createDatasetResource(dataset) } }
+        }
+
+        return model
     }
-
-    return model
 }
+
