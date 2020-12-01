bda114c1b819c3527d87b97a0b96fc98952e9846
matsjsk <matsjorgen@hotmail.com>
2020-12-01 12:45:37 +0100

    fix: adds isomorphism test

--- src/test/kotlin/no/fdk/dataset_catalog/contract/RdfContractTest.kt
+++ src/test/kotlin/no/fdk/dataset_catalog/contract/RdfContractTest.kt
@@ -1,20 +1,18 @@
 package no.fdk.dataset_catalog.contract
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.fdk.dataset_catalog.utils.*
-import no.fdk.dataset_catalog.utils.jwk.Access
-import no.fdk.dataset_catalog.utils.jwk.JwtToken
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Tag
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
+import org.apache.jena.rdf.model.ModelFactory
+import org.junit.jupiter.api.*
+import org.slf4j.LoggerFactory
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
+import java.io.StringReader
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
-private val mapper = jacksonObjectMapper()
+private val logger = LoggerFactory.getLogger(RdfContractTest::class.java)
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 @SpringBootTest(
@@ -23,15 +21,28 @@ private val mapper = jacksonObjectMapper()
 @ContextConfiguration(initializers = [ApiTestContext.Initializer::class])
 @Tag("contract")
 class RdfContractTest: ApiTestContext() {
+    private val responseReader = TestResponseReader()
+
     @Nested
     internal inner class GetRDF {
         @Test
         fun `Gets Catalog`() {
+            resetDB()
+
             val getAll = apiAuthorizedRequest("/catalogs/", method="GET", accept=MediaType("text","turtle"))
             val getOne = apiAuthorizedRequest("/catalogs/$DB_CATALOG_ID_1", method="GET", accept=MediaType("text","turtle"))
 
             assertEquals(HttpStatus.OK.value(), getAll["status"])
             assertEquals(HttpStatus.OK.value(), getOne["status"])
+
+            val expectedGetAll = responseReader.parseFile("getAll.ttl", "TURTLE")
+            val expectedGetOne = responseReader.parseFile("getOne.ttl", "TURTLE")
+
+            val actualGetAll = ModelFactory.createDefaultModel().read(StringReader(getAll["body"] as String), null, "TURTLE")
+            val actualGetOne = ModelFactory.createDefaultModel().read(StringReader(getOne["body"] as String), null, "TURTLE")
+
+            assertTrue(checkIfIsomorphicAndPrintDiff(actualGetAll, expectedGetAll, "Get All result", logger))
+            assertTrue(checkIfIsomorphicAndPrintDiff(actualGetOne, expectedGetOne, "Get One result", logger))
         }
     }
 }
