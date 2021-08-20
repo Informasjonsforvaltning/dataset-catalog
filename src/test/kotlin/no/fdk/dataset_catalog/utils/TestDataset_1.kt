fb7a21208f4bfaf3e460630ed90a77e64b4042a7
NilsOveTen <github.nils.ove@tendenes.com>
2021-08-20 14:11:10 +0200

    fix: serialize distribution.format as uris

--- src/test/kotlin/no/fdk/dataset_catalog/utils/TestDataset_1.kt
+++ src/test/kotlin/no/fdk/dataset_catalog/utils/TestDataset_1.kt
@@ -97,14 +97,14 @@ val DISTRIBUTION = Distribution(
     conformsTo= listOf(CONFORMS_TO),
     license=SkosConcept("https://data.norge.no/nlod/no/2.0", mapOf(Pair("nb", "NODL")), extraType = DCTerms.LicenseDocument.uri),
     page= listOf(SkosConcept("http://lenke/til/mer/info",  mapOf(Pair("nb", "Dokumentasjon av distribusjonen")), extraType =  FOAF.Document.uri)),
-    format=listOf("application/json"),
+    format=listOf("https://www.iana.org/assignments/media-types/application/json"),
     accessService=listOf(SAMPLE_DESCRIPTION)
 )
 
 val SAMPLE = Distribution(
     uri = "$DATASET_URI/samples/d2",
     description = mapOf(Pair("nb", "Dette er beskrivelsen av eksempeldataene. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.")),
-    format =  listOf("application/rdf+xml"),
+    format =  listOf("https://www.iana.org/assignments/media-types/application/rdf+xml"),
     accessURL =  listOf("http://www.detteerenlenke.no/til-nedlasting", "www.dette.kan.også/hende")
 )
 
