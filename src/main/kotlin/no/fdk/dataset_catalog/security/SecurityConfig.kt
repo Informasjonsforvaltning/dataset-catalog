26e9f21f31e4114b7d66d79efc81f52ed5f658eb
Nils Ove Tendenes <nils.ove@tendenes.com>
2026-03-24 14:55:28 +0100

    fix: disable csrf

--- src/main/kotlin/no/fdk/dataset_catalog/security/SecurityConfig.kt
+++ src/main/kotlin/no/fdk/dataset_catalog/security/SecurityConfig.kt
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
 import org.springframework.security.oauth2.jwt.*
 import org.springframework.security.web.SecurityFilterChain
@@ -19,6 +20,8 @@ open class SecurityConfig(
     @Bean
     open fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
+            .csrf { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             .cors { cors ->
                 cors.configurationSource { _ ->
                     val config = CorsConfiguration()
